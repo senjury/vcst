949450d4167c4a0e1e7afea181de08a981b8bbec
bob <bob@IV>
2015-09-22 22:15:23 +0800

    米库初始化

---
+++ pg/pg/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pg
+//
+//  Created by Robert Suen on 15/7/20.
+//  Copyright (c) 2015年 onev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
